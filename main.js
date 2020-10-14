4e603a7c0de48af6702079f97864395d8ccbbc35
Maurizio <maurizio.faedda1@gmail.com>
2020-10-14 18:28:27 +0200

    algoritmo email ok

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 // chiedo l'email all'utente
-var emailUtente = prompt("Inserisci la tua email")
+var emailUtente = prompt("Inserisci la tua email");
 
 // le mia lista di email
-var emailRegistrate = ['andrea@gmail.com', 'marco@gmail.com', 'sara@gmail.com', 'giulia@gmail.com']
+var emailRegistrate = ['andrea@gmail.com', 'marco@gmail.com', 'sara@gmail.com', 'giulia@gmail.com'];
 
-// console.log(emailRegistrate);
+var emailVerificata = false;
 
 // Cerco se l'email inserita è presente nella lista
 for (var i = 0; i < emailRegistrate.length; i++) {
 
-    if (emailRegistrate[i] !== emailUtente) {
-        console.log("la tua email Non è presente nella nostra lista");
-    } else {
-        console.log("la tua email è presente nella nostra lista");
+    if (emailRegistrate[i] == emailUtente) {
+        var emailVerificata=true;
     }
+}
+
+if (emailVerificata == true) {
+    console.log("la tua email è presente");
+} else  {
+    console.log("la tua email non è verificata");
 }
