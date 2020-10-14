0b369b16d969fdc0924005d79776a0465a42892c
Maurizio <maurizio.faedda1@gmail.com>
2020-10-14 18:41:44 +0200

    Lancio dei dadi
    completato algoritmo vittoria pareggio sconfitta

--- main.js
+++ main.js
@@ -1,21 +1,42 @@
-// chiedo l'email all'utente
-var emailUtente = prompt("Inserisci la tua email");
+// -------------check email------------
 
-// le mia lista di email
-var emailRegistrate = ['andrea@gmail.com', 'marco@gmail.com', 'sara@gmail.com', 'giulia@gmail.com'];
+// // chiedo l'email all'utente
+// var emailUtente = prompt("Inserisci la tua email");
+//
+// // le mia lista di email
+// var emailRegistrate = ['andrea@gmail.com', 'marco@gmail.com', 'sara@gmail.com', 'giulia@gmail.com'];
+//
+// var emailVerificata = false;
+//
+// // Cerco se l'email inserita è presente nella lista
+// for (var i = 0; i < emailRegistrate.length; i++) {
+//
+//     if (emailRegistrate[i] == emailUtente) {
+//         var emailVerificata=true;
+//     }
+// }
+//
+// if (emailVerificata == true) {
+//     console.log("la tua email è presente");
+// } else  {
+//     console.log("la tua email non è verificata");
+// }
 
-var emailVerificata = false;
 
-// Cerco se l'email inserita è presente nella lista
-for (var i = 0; i < emailRegistrate.length; i++) {
+// --------lancio dadi----------
 
-    if (emailRegistrate[i] == emailUtente) {
-        var emailVerificata=true;
-    }
-}
+// lancio il dado 1
+var lancioUno = Math.floor(Math.random() * 6) + 1;
+console.log("Giocatore Uno il tuo punteggio è " + lancioUno);
 
-if (emailVerificata == true) {
-    console.log("la tua email è presente");
-} else  {
-    console.log("la tua email non è verificata");
+// lancio il dado 2
+var lancioDue = Math.floor(Math.random() * 6) + 1;
+console.log("Giocatore Due il tuo punteggio è " + lancioDue);
+
+if (lancioUno > lancioDue) {
+    console.log("Giocatore Uno hai vinto!");
+} else if (lancioUno < lancioDue) {
+    console.log("Giocatore Due hai vinto!");
+} else if (lancioUno == lancioDue) {
+    console.log("Avete pareggiato, nessuno dei due vince");
 }
